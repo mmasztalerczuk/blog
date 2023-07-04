299330450894e37e20bd6999eb0fadf14c316cea
Mariusz Masztalerczuk <mariusz@masztalerczuk.com>
2023-07-04 21:51:19 +0200

    Structured logging in GCP

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,10 +1,10 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Dev blog',
+  author: 'Mariusz Masztalerczuk',
+  headerTitle: 'Mariusz - dev blog',
+  description: 'Blog "mostly" about Python..',
   language: 'en-us',
-  theme: 'system', // system, dark or light
+  theme: 'dark', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
